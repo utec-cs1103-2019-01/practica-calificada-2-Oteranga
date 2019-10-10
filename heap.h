0ce6ec0564f5d07e63890e0ffa8b56615c04a6a2
Ruben Rivas <rrivas@utec.edu.pe>
2019-10-10 14:24:09 -0500

    Initial commit

---
+++ heap.h
@@ -0,0 +1,8 @@
+//
+// Created by rudri on 10/10/2019.
+//
+
+#ifndef CS1103_PC2_201902_HEAP_H
+#define CS1103_PC2_201902_HEAP_H
+
+#endif //CS1103_PC2_201902_HEAP_H
---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "heap.h"
+using namespace std;
+
+int main()
+{
+    heap<int> h;
+    load_from(string("data.txt"), h);
+    cout << "heap" << endl;
+    cout << h;
+    h.insert(10);
+    h.insert(20);
+    h.insert(40);
+    h.insert(50);
+    h.insert(10);
+    cout << "heap" << endl;
+    cout << h;
+    cout << "heap" << endl;
+    cout << h;
+    cin >> h;
+    cout << "heap" << endl;
+    cout << h;
+    insert(h, 100, 200);
+    cout << "heap" << endl;
+    cout << h;
+    load_from("data.txt", h);
+    return 0;
+}
